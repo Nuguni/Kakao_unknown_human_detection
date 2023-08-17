848a2b8fb60b70137197564d6d62f236c45852b2
2youngjun <86882798+2youngjun@users.noreply.github.com>
2023-08-17 17:58:19 +0900

    [Feat] Init Server

---
+++ Server/nuguni/src/main/java/bob/nuguni/NuguniApplication.java
@@ -0,0 +1,13 @@
+package bob.nuguni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuguniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuguniApplication.class, args);
+	}
+
+}
---
+++ Server/nuguni/src/test/java/bob/nuguni/NuguniApplicationTests.java
@@ -0,0 +1,13 @@
+package bob.nuguni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuguniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
